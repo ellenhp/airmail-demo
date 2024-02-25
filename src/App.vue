1252f957100ea6e84f3aaab4f7a8da3fe7e97a33
Ellen Poe <ellen.h.poe@gmail.com>
2024-02-25 08:35:16 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+import Explanation from "./components/Explanation.vue";
+import Features from "./components/Features.vue";
+import Roadmap from "./components/Roadmap.vue";
+import Demo from "./components/Demo.vue";
+</script>
+
+<template>
+  <Header />
+  <Hero />
+  <Explanation />
+  <Features />
+  <Roadmap />
+  <Demo />
+</template>
